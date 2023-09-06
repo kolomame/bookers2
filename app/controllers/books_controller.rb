74445bd674848b14ea1f717c0d627e66ddffed4a
EC2 Default User <ec2-user@ip-172-31-1-135.ap-northeast-1.compute.internal>
2023-09-06 20:03:20 +0000

    [Add]4-27

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,19 +7,37 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      @book_new = Book.new
+      @user_new = current_user
+      render :index
+    end
   end
 
   def index
+    @book = Book.new
     @books = Book.all
+    @book_new = Book.new
+    @user_new = current_user
   end
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
+    @user_new = current_user
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get 'homes/about', to: 'homes#about', as: :about
-  resources :books, only: [:new, :create, :index, :show]
-  resources :users, only: [:index, :show, :edit]
+  resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
 
 
 
